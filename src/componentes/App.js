4e891a36a8b754a4aead4125d9b9f7cb91a25480
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-07-11 18:25:43 -0300

    update App.js

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,18 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import Produtos from "./Produtos/Produtos";
 import Carrinho from "./Carrinho/Carrinho";
 import Checkout from "./CheckOut/Checkout";
 
-export default function App () {
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Produtos/>} />
-					<Route path="/carrinho" element={<Carrinho/>} />
-					<Route path="/checkout" element={<Checkout/>} />
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+import Login from "./components/Login";
+import SignUp from "./components/Sign-Up";
+import GlobalStyle from "./assets/GlobalStyle/GlobalStyle";
+import UserContext from "./contexts/UserContext";
+
+export default function App() {
+    const [user, setUser] = useState({});
+
+    function Error(e) {
+        console.log(`${e.response.status} - ${e.response.statusText}`);
+        alert("Um erro aconteceu, tente novamente");
+    }
+
+    return (
+        <UserContext.Provider value={{
+            user,
+            setUser,
+            Error
+        }}>
+            <BrowserRouter>
+                <GlobalStyle />
+                    <Routes>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route path="/" element={<Produtos/>} />
+                        <Route path="/carrinho" element={<Carrinho/>} />
+                        <Route path="/checkout" element={<Checkout/>} />
+                    </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
+    );
 }
